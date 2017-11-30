88ee1e3d96174634ec6934e60c48cebf90118a7b
Jurian <jurian@jurianbaas.nl>
2017-11-30 23:34:56 +0100

    Added basic weighted tree implementation

--- src/main/java/com/hnunner/graphstreamsim/MultiThreadSim.java
+++ src/main/java/com/hnunner/graphstreamsim/MultiThreadSim.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
